de5af26b91c8b839e1c39d5deb48674a38a20b34
Faizaan <alifaiza@grinnell.edu>
2016-03-22 16:09:52 -0500

    testing GIT

--- src/main/kotlin/com/vocalabs/age/Simulator.kt
+++ src/main/kotlin/com/vocalabs/age/Simulator.kt
@@ -8,11 +8,16 @@ class Simulator {
     val person2 = Person(1, 2007)
     var finalList = mutableListOf(person1, person2)
     var tempList = listOf(person1, person2)
-    fun spawnSimulator ()  {
+    fun spawnSimulator (): MutableList<Person> {
         for (i in 1..7)  {
             tempList = tempList.flatMap { it.spawn() }
             finalList.addAll(tempList)
         }
+        return finalList
     }
 
+}
+
+fun main (args:Array<String>){
+    println(Simulator().spawnSimulator())
 }
