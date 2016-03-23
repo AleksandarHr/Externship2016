457b410f118d1497a361a30ec732e02f4c95cc7a
Aleksandar <hrusanov@grinnell.edu>
2016-03-23 22:12:25 +0200

    Small Changes

--- src/main/kotlin/com/vocalabs/age/NewPerson.kt
+++ src/main/kotlin/com/vocalabs/age/NewPerson.kt
@@ -1,8 +1,56 @@
 package com.vocalabs.age
+import java.lang.Math
+import java.util.Random
 
 /**
  * Created by Aleksandar on 23.3.2016 г..
  */
 class NewPerson (val generation: Int, var dateOfBirth: Int) {
 
+    fun willGiveBirth (random: Random): Boolean {
+        var chance = random.nextDouble()
+
+        if (chance > 0.47) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    fun ageOfFirstBirth (random: Random): Int {
+        var age: Int
+
+        age = Math.round(random.nextGaussian() * 12 + 27).toInt()
+        return age
+    }
+
+    fun calculateNumberOfChildren(random: Random): Int {
+        var numOfChildren = 0
+        var gaussian = random.nextGaussian() * 1 + 2.5
+        if (gaussian >= 0.0 && gaussian <= 5.0) {
+            numOfChildren = Math.round(gaussian).toInt()
+        }
+        return numOfChildren
+    }
+
+    fun giveBirth (numberOfKids: Int, firstBirthAge: Int): kotlin.collections.MutableList<Person> {
+        var newGeneration = mutableListOf<Person>()
+        if (numberOfKids == 0) {
+        }
+        else {
+            for (i in 0..numberOfKids) {
+                var child = Person(this.generation + 1, firstBirthAge)
+                when (i) {
+                    1 -> child.dateOfBirth = firstBirthAge
+                    2 -> child.dateOfBirth = firstBirthAge + 3
+                    3 -> child.dateOfBirth = firstBirthAge + 4
+                    4 -> child.dateOfBirth = firstBirthAge + 5
+                    5 -> child.dateOfBirth = firstBirthAge + 8
+                }
+                newGeneration.add(child)
+            }
+        }
+        return newGeneration
+    }
 }
--- src/main/kotlin/com/vocalabs/age/NewSimulator.kt
+++ src/main/kotlin/com/vocalabs/age/NewSimulator.kt
@@ -6,5 +6,16 @@ import com.vocalabs.util.histogram
  * Created by Aleksandar on 23.3.2016 г..
  */
 class NewSimulator {
+    val parent = NewPerson(1, 2003)
 
+}
+
+fun main (args:Array<String>){
+    val people = Simulator().spawnSimulator()
+    for (i in 1..7) {
+        val peopleInGeneration = people.filter { it.generation == i }
+        println("\n\n----- Generation $i ----")
+        val datesOfBirth: List<Int> = peopleInGeneration.map{it.dateOfBirth}
+        println(histogram(datesOfBirth))
+    }
 }
