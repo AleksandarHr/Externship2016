be4aef9055ace114d0647124f913d319b63dc144
David Leppik <dleppik@vocalabs.com>
2016-03-23 10:39:07 -0500

    Added unit test support.

---
+++ src/test/kotlin/com/vocalabs/age/PersonTest.kt
@@ -0,0 +1,12 @@
+package com.vocalabs.age
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class PersonTest() {
+
+    @Test
+    fun noTestsAreWritten() {
+        fail("No tests have been written")
+    }
+}
